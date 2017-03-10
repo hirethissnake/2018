7d6f7fbae44566110a3ec53d9f146185d1cbddfd
Daniel Frankcom <danielfrankcom@gmail.com>
2017-03-10 09:54:34 -0800

    Append column to right of table

--- testgenerator/script.js
+++ testgenerator/script.js
@@ -3,19 +3,20 @@ var foodColour = "#4CAF50";
 var youColor = "#E91E63";
 var snakeColours = ["#9C27B0", "#3F51B5", "#03A9F4", "#009688", "#CDDC39", "#FFEB3B", "#FF9800", "#FF5722", "#795548", "#607D8B"];
 
+var windowSize = 0;
 
 function main(){
 
   var width = window.innerWidth;
   var height = window.innerHeight;
-  var boundary = 0;
   if(width < height){
-    boundary = width;
+    windowSize = width - 100;
   }else{
-    boundary = height;
+    windowSize = height - 100;
   }
 
-  tableCreate(20, boundary);
+  tableCreate(20);
+  appendColumn();
 
   //var table = document.getElementById("table");
 }
@@ -29,19 +30,17 @@ function cellClicked(elem){
 }
 
 
-function tableCreate(tableSize, windowSize) {
+function tableCreate(tableSize) {
 
   var body = document.getElementsByTagName('body')[0];  // get body
 
   var tbl = document.createElement('table');  // declare table
   tbl.setAttribute('border', '1');
   tbl.setAttribute('id', 'table');
-  tbl.style.width = (windowSize - 50) + 'px';
+  tbl.style.width = (2 * (windowSize - 50)) + 'px';
   tbl.style.height = (windowSize - 50) + 'px';
 
   var tbdy = document.createElement('tbody');  // declare body
-
-  var cellSize = (100 / tableSize) + '%';  // declare cell size
 
   for (var row = 0; row < tableSize; row++) {  // for table size
 
@@ -52,8 +51,8 @@ function tableCreate(tableSize, windowSize) {
       var td = document.createElement('td');
       td.setAttribute('id', row + ',' + col);  // set name
       td.setAttribute('onclick', 'cellClicked(this)');  // make clickable
-      td.style.width = cellSize;
-      td.style.height = cellSize;
+      td.style.width = ((100 / tableSize) / 2) + "%";
+      td.style.height = (100 / tableSize) + "%";
 
       td.appendChild(document.createTextNode('\u0020'))  // add empty text
 
@@ -68,4 +67,23 @@ function tableCreate(tableSize, windowSize) {
   tbl.appendChild(tbdy);  // add table to HTML
   body.appendChild(tbl);
 
+}
+
+function createCell(cell) {
+
+  var div = document.createElement('div'); // create DIV element
+  div.appendChild(document.createTextNode('\u0020'));  // append text node to the DIV
+  cell.setAttribute('rowspan', 20);
+  div.style.width = (windowSize / 2) + "px";
+  cell.appendChild(div);                   // append DIV to the table cell
+
+}
+
+function appendColumn() {
+  var tbl = document.getElementById('table'), // table reference
+  // open loop for each row and append cell
+  i;
+  for (i = 0; i < tbl.rows.length; i++) {
+    if(i==0){createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length));}
+  }
 }
