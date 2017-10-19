d407efbc0713b62ae72ee5da06e66333de29d9aa
Daniel Frankcom <danielfrankcom@gmail.com>
2017-10-19 14:31:20 -0700

    added some empty methods, and updated the argument specification further

--- utilities/batchserver/main.py
+++ utilities/batchserver/main.py
@@ -3,6 +3,22 @@
 if __name__ == '__main__':
     print("We're up!")
 
+def printGames(games, p, m):
+    #given games[], print to file/directory p
+    #if m(bool), print each game to a different file
+    return
+
+def stepSnakes(gameState):
+    #send gameState to each snake and waits for a response
+    return
+
+def incrementState(snake, move):
+    #increments snake position in game state using move
+    return
+
+def generateFood(numItems):
+    #check to see if a random food item is due to be added
+    return
 
 ## Accept command inputs ##
 #-s 'game state to run from'
@@ -10,6 +26,10 @@ if __name__ == '__main__':
 #-n 'number of games to run'
 #   will have hard upper limit
 #-p 'path for game outcomes'
+#   if not present, use default
+#-m 'print separate files for each game'
+#   if not present, all games in one file
+#-f 'number of food items at any one time'
 #   if not present, use default
 
 ## Generate game state to send to snakes ##
