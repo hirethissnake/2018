f2da1a5f1c431948236097ec50f6cd482636d9d9
Daniel Frankcom <danielfrankcom@gmail.com>
2017-10-19 14:23:52 -0700

    Sketched basic layout for file, what needs to be done

--- utilities/batchserver/main.py
+++ utilities/batchserver/main.py
@@ -2,3 +2,16 @@
 
 if __name__ == '__main__':
     print("We're up!")
+
+
+## Accept command inputs ##
+#-s 'game state to run from'
+#   if not present, use default
+#-n 'number of games to run'
+#   will have hard upper limit
+#-p 'path for game outcomes'
+#   if not present, use default
+
+## Generate game state to send to snakes ##
+## Given a move, modify the game state to match ##
+## Build in local snakes and web snakes ##
