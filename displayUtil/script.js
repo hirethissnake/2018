296d6e690f512b6bd5a89fa4cf95bea5d93a96fa
Eric-Showers <ejs1357@hotmail.com>
2017-10-21 14:25:22 -0700

    Display util now working for multi turns, 3 sec delay

--- displayUtil/script.js
+++ displayUtil/script.js
@@ -1,53 +1,133 @@
+var snakeColours = ["#4CAF50","#607D8B","#E91E63","#9C27B0", "#3F51B5", "#03A9F4", "#009688", "#CDDC39", "#FFEB3B", "#FF9800", "#FF5722", "#795548"];
+var snakesHead = [false, true, true, true, true, true, true, true, true, true, true, true, true];
 
+var currentSelected = 0;
+var tableSize = 20;
+var mouseDown = 0;  // store mouse status
+var width = 0;
+var height = 0;
+var tableWidth = 0;
+var tableHeight = 0;
+var turnCounter = 0;
+var interID;
+
+var oneTurnData = {
+    'you':'you',
+    'turn':1,
+    'snakes':[
+        {'taunt':'gotta go slow',
+        'name':'snake1',
+        'id':'1',
+        'health_points':100,
+        'coords':[[7,2],[7,3],[7,4],[7,5],[7,6],[8,6]]},
+        {'taunt':'gotta go slow',
+        'name':'snake4',
+        'id':'4',
+        'health_points':100,
+        'coords':[[4,11],[5,11],[6,11],[7,11],[7,12],[7,13],[8,13],[8,14],[8,15],[9,15]]},
+        {'taunt':'gotta go slow',
+        'name':'snake5',
+        'id':'5',
+        'health_points':100,
+        'coords':[[16,13],[12,9],[13,9],[14,9],[15,9],[16,9],[12,10],[16,10],[12,11],[13,11],[13,12],[14,12],[15,13]]}
+    ],
+    'height':20,
+    'width':20,
+    'game_id':'gameid',
+    'food':[
+        [14,2],
+        [3,6],
+        [18,11],
+        [12,13],
+        [3,16]
+    ],
+    'dead_snakes':[]
+};
+var twoTurnData = [
+    {"you":"you","turn":1,"snakes":[{"taunt":"gotta go fast","name":"sneakysnake","id":"you","health_points":100,"coords":[]},{"taunt":"gotta go slow","name":"snake1","id":"1","health_points":100,"coords":[[7,2],[7,3],[7,4],[7,5],[7,6],[8,6]]},{"taunt":"gotta go slow","name":"snake4","id":"4","health_points":100,"coords":[[4,11],[5,11],[6,11],[7,11],[7,12],[7,13],[8,13],[8,14],[8,15],[9,15]]},{"taunt":"gotta go slow","name":"snake5","id":"5","health_points":100,"coords":[[16,13],[12,9],[13,9],[14,9],[15,9],[16,9],[12,10],[16,10],[12,11],[13,11],[13,12],[14,12],[15,13]]}],"height":20,"width":20,"game_id":"gameid","food":[[14,2],[3,6],[18,11],[12,13],[3,16]],"dead_snakes":[]},
+    {"you":"you","turn":2,"snakes":[{"taunt":"gotta go fast","name":"sneakysnake","id":"you","health_points":100,"coords":[]},{"taunt":"gotta go slow","name":"snake1","id":"1","health_points":100,"coords":[[7,1],[7,2],[7,3],[7,4],[7,5],[7,6]]},{"taunt":"gotta go slow","name":"snake4","id":"4","health_points":100,"coords":[[4,10],[4,11],[5,11],[6,11],[7,11],[7,12],[7,13],[8,13],[8,14],[8,15]]},{"taunt":"gotta go slow","name":"snake5","id":"5","health_points":100,"coords":[[17,13],[12,9],[13,9],[14,9],[15,9],[16,9],[12,10],[12,11],[13,11],[13,12],[14,12],[15,13],[16,13]]}],"height":20,"width":20,"game_id":"gameid","food":[[14,2],[3,6],[18,11],[12,13],[3,16]],"dead_snakes":[]}
+];
+
+function main() {
+    
+    width = window.innerWidth - 50;
+    height = window.innerHeight - 50;
+
+    tableCreate();
+    interID = setInterval(newTurn, 3000);
+    console.log(twoTurnData.length);
+
+}
 
 function tableCreate() {
     
-      var body = document.getElementsByTagName("body")[0];  // get body
-    
-      var tbl = document.createElement("table");  // declare table
-      tbl.setAttribute("border", "1");
-      tbl.setAttribute("id", "table");
-    
-      if(width > height){
+    var body = document.getElementsByTagName("body")[0];  // get body
+
+    var tbl = document.createElement("table");  // declare table
+    tbl.setAttribute("border", "1");
+    tbl.setAttribute("id", "table");
+
+    if(width > height){
         var desiredWidth = width;
         if(height < width / 2){
-          tableWidth = tbl.style.width = (height * 2);
+          tableWidth = tbl.style.width = height;
           tableHeight = tbl.style.height = height;
         }else{
-          tableWidth = tbl.style.width = width;
+          tableWidth = tbl.style.width = width / 2;
           tableHeight = tbl.style.height = (width / 2);
         }
-      }else{
+    }else{
         tableWidth = tbl.style.width = width;
-        tableHeight = tbl.style.height = (width / 2);
-      }
-    
-      var tbody = document.createElement("tbody");  // declare body
-    
-      for (var row = 0; row < tableSize; row++) {  // for table size
-    
-        var tableRow = document.createElement("tr");  // declare row
-    
-        for (var col = 0; col < tableSize; col++) {
-    
-          var rowCell = document.createElement("rowCell");
-          rowCell.setAttribute("id", col + "," + row);  // set name
-          rowCell.setAttribute("class", "selectionSquare");
-    
-          rowCell.style.width = ((100 / tableSize) / 2) + "%";
-          rowCell.style.height = (100 / tableSize) + "%";
-    
-          rowCell.appendChild(document.createTextNode("\u0020"))  // add empty text
-    
-          tableRow.appendChild(rowCell)  // append cell to row
-    
-        }
-    
-        tbody.appendChild(tableRow);  // append row to table
-    
-      }
-    
-      tbl.appendChild(tbody);  // add table to HTML
-      body.appendChild(tbl);
-    
+        tableHeight = tbl.style.height = (width);
     }
+
+    var tbdy = document.createElement("tbody");  // declare body
+
+    for (var row = 0; row < tableSize; row++) {  // for table size
+
+    var tr = document.createElement("tr");  // declare row
+
+    for (var col = 0; col < tableSize; col++) {
+
+        var td = document.createElement("td");
+        td.setAttribute("id", col + "," + row);  // set name
+        td.setAttribute("class", "selectionSquare");
+
+        td.style.width = ((100 / tableSize) / 2) + "%";
+        td.style.height = (100 / tableSize) + "%";
+
+        td.appendChild(document.createTextNode("\u0020"))  // add empty text
+
+        tr.appendChild(td)  // append cell to row
+
+    }
+
+    tbdy.appendChild(tr);  // append row to table
+
+    }
+
+    tbl.appendChild(tbdy);  // add table to HTML
+    body.appendChild(tbl);
+
+}
+
+function newTurn() {
+    console.log("Woahboi "+turnCounter);
+    var thisTurn = twoTurnData[turnCounter];
+    
+    for (x in thisTurn.food){
+        var kibbles = thisTurn.food[x];
+        document.getElementById(kibbles[0]+','+kibbles[1]).style.backgroundColor = "rgb(76, 175, 80)";
+    }
+    for (x in thisTurn.snakes){
+        var curSnake = thisTurn.snakes[x];
+        for (y in curSnake.coords){
+            var curCoord = curSnake.coords[y];
+            document.getElementById(curCoord[0]+','+curCoord[1]).style.backgroundColor = snakeColours[curSnake.id];
+        }
+    }
+    turnCounter++;
+    if (turnCounter >= twoTurnData.length) {
+        clearInterval(interID);
+    }
+}
