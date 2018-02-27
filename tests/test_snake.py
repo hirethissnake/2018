94dc3d1933664c907eb3e4961e2d11cde3e7af25
Daniel Frankcom <danielfrankcom@gmail.com>
2018-02-26 22:28:47 -0800

    Fixed state test following removal of method

--- tests/test_snake.py
+++ tests/test_snake.py
@@ -25,7 +25,6 @@ class TestSnake(unittest.TestCase):
         s1 = Snake(initParams)
 
         size_expected = len(initParams['coords'])
-        state_expected = 'unknown'
         health_expected = initParams['health_points']
         headPosition_expected = initParams['coords'][0]
         tailPosition_expected = initParams['coords'][-1]
@@ -33,33 +32,11 @@ class TestSnake(unittest.TestCase):
         identifier_expected = initParams['id']
 
         self.assertEqual(s1.getSize(), size_expected)
-        self.assertEqual(s1.getState(), state_expected)
         self.assertEqual(s1.getHealth(), health_expected)
         self.assertEqual(s1.getHeadPosition(), headPosition_expected)
         self.assertEqual(s1.getTailPosition(), tailPosition_expected)
         self.assertEqual(s1.getAllPositions(), positions_expected)
         self.assertEqual(s1.getIdentifier(), identifier_expected)
-
-    def test_setState(self):
-        """
-        Test the setState function
-        """
-        initParams = {
-            'id': 's1',
-            'coords': [
-                [0, 1],
-                [1, 1]
-            ],
-            'health_points': 75
-        }
-        s1 = Snake(initParams)
-
-        state_expected = 'food'
-        s1.setState(state_expected)
-        self.assertEqual(s1.getState(), state_expected)
-
-        with self.assertRaises(ValueError):
-            s1.setState('watermelon')
 
     def test_updates_valid(self):
         """
